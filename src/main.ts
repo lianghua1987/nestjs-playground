1105ba3af66afd94ff954ad9203d0e67af1a17f8
lianghua1987 <hua_liang@mckinsey.com>
2023-10-07 23:03:00 +0800

    Create using cli

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,8 @@
-import {Controller, Module, Get} from "@nestjs/common";
-import {NestFactory} from "@nestjs/core";
-
-@Controller()
-class AppController {
-
-  @Get()
-  getRootRoute() {
-    return 'hi there';
-  }
-}
-
-@Module({
-  controllers: [AppController]
-})
-class AppModule {
-
-}
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-
 bootstrap();
